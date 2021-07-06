a941f4281592b7c255019dae848e41f14fa2f756
vincent blanchet <vblanchet@ippon.fr>
2021-07-06 15:18:15 +0200

    first commit

---
+++ sonarTest/sonarTestApp.swift
@@ -0,0 +1,24 @@
+//
+//  sonarTestApp.swift
+//  sonarTest
+//
+//  Created by Vincent BLANCHET on 06/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SonarTestApp: App {
+    var myVar: Int?
+    var myArray = ["one"]
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+
+    func toto(index: Int) {
+        let myLet = myVar!
+        let myString = myArray[index]
+    }
+}
